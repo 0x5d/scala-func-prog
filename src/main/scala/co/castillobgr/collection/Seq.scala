bd977101b8487f99fad6e2d99f603fc4cb7f76cd
David Castillo <castillobgr@gmail.com>
2016-10-13 22:04:52 -0500

    Restructure folders

--- src/main/scala/co/castillobgr/collection/Seq.scala
+++ src/main/scala/co/castillobgr/collection/Seq.scala
@@ -1,9 +1,5 @@
 package co.castillobgr.collection
 
-import co.castillobgr.Option
-import co.castillobgr.Some
-import co.castillobgr.None
-
 object Seq {
 
   def mean(xs: Seq[Double]): Option[Double] =
